fddad6927b9efbd2e4cfc9f1d5598e8fba37c411
yanglei <yanglei50070@163.com>
2020-10-29 17:42:36 +0800

    s

---
+++ atguigu/src/main/java/wordcount/WordCountMapper.java
@@ -0,0 +1,24 @@
+package wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+    private Text k = new Text();
+    private  IntWritable v = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String s = value.toString();
+        String[] split = s.split(" ");
+        for (String s1 : split) {
+            k.set(s1);
+            context.write(k,v);
+        }
+
+    }
+}
